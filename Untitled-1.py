0ce58575af3c4bed0ffd974da40f26851a3d1ad9
Nafery <nacho.frivera1@gmail.com>
2023-05-25 14:39:11 -0300

    Clase 2505

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+numero=int(input("ingrese un numero: "))
+lista=[]
+for i in range (1,11):
+    tabla=numero*i
+    lista.append(tabla)
+
+contador=1
+for x in lista:
+    print(f"Posicion {contador} es: {x}")
+    contador+=1
